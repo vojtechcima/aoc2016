2310b4ac6d70419a7cc76a7b546620743f417b41
Vojtech Cima <cima.vojtech@gmail.com>
2016-12-06 10:47:11 +0100

    [src] added Day 6 solution

---
+++ 6/1.py
@@ -0,0 +1,30 @@
+def read_lines(path):
+    lines = []
+    with open(path, "r") as f:
+        for l in f.readlines():
+            lines.append(l.strip())
+    return lines
+
+
+def init_counters(n):
+    counters = []
+    for i in range(n):
+        counters.append({})
+    return counters
+
+
+def get_max(d):
+    return d.keys()[d.values().index(max(d.values()))]
+
+lines = read_lines("input.txt")
+
+counter = init_counters(len(lines[0]))
+for l in lines:
+    for ic in range(len(l)):
+        if l[ic] not in counter[ic]:
+            counter[ic][l[ic]] = 0
+        counter[ic][l[ic]] += 1
+
+result = "".join([get_max(c) for c in counter])
+
+print result
